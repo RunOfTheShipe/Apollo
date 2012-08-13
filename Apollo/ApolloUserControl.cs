18def3e389daa6fdea761b4f8fbf567a1fa4f5be
Brian Shipe <bmshipe@gmail.com>
2012-08-12 21:36:38 -0500

    More work.

---
+++ Apollo/ApolloUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+using Apollo.ViewModel;
+
+namespace Apollo.View
+{
+    public class ApolloUserControl<T> : UserControl
+        where T : ApolloViewModelBase
+    {
+        public T Presentation
+        {
+            get { return (T)DataContext; }
+            set { DataContext = value; }
+        }
+    }
+}
---
+++ Apollo/Controls/CreateRaceControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Apollo.ViewModel;
+
+namespace Apollo.View.Controls
+{
+    /// <summary>
+    /// Non-class.  Since ApolloUserControl is a generic class, it can't be defined in XAML (maybe it can, but don't
+    /// know how).  Define this wrapper class to allow us to create the derived type in XAML.
+    /// </summary>
+    public abstract class CreateRaceControlType : ApolloUserControl<CreateModifyRaceViewModel>
+    { }
+
+    /// <summary>
+    /// Interaction logic for CreateRaceControl.xaml
+    /// </summary>
+    public partial class CreateRaceControl : CreateRaceControlType
+    {
+        public CreateRaceControl()
+        {
+            //? How to get the Apollo.Model in here?
+            Presentation = new CreateModifyRaceViewModel(null, null);
+            InitializeComponent();
+        }
+    }
+}
